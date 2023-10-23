ed2562e89f9a90d3c266d783c2242ea26a5faa22
t.nugent <t.nugent@hotmail.co.uk>
2023-10-23 14:15:36 +0100

    Add project files.

---
+++ 2022- Day 11 - Monkeys/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string path = @"C:\Users\Tom\Documents\ASPNET Projects\2022- Day 11 - Monkeys\2022- Day 11 - Monkeys\data_test.txt";
+
+        string[] instructions = System.IO.File.ReadAllLines(path);
+
+        foreach(string instruction in instructions)
+        {
+            Console.WriteLine(instruction);
+        }
+    }
+}
